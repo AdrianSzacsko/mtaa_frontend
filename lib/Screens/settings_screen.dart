7c30b91bb909991351413b3a08a9c39f4abf0de0
Adrian Szacsko <szacskoadrian@gmail.com>
2022-04-08 13:09:33 +0200

    modify settings_screen.dart

--- lib/Screens/settings_screen.dart
+++ lib/Screens/settings_screen.dart
@@ -7,6 +7,8 @@ import 'package:mtaa_frontend/UI/inputField.dart';
 import 'package:mtaa_frontend/Screens/search_screen.dart';
 import 'package:mtaa_frontend/Screens/sign_in_screen.dart';
 
+import '../constants.dart';
+
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
@@ -61,6 +63,115 @@ class SettingsScreenState extends State<SettingsScreen> {
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => SearchScreen()));
               },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.lightBlueAccent,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "   Settings:",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+            ),
+            const Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  //TODO add info
+                },
+                child: const Text("Info",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  //TODO add webrtc call screen
+                },
+                child: const Text("Call Support",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return Dialog(
+                          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                          elevation: 16,
+                          child: Container(
+                            margin: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: const Text("Are you sure you want to permanently delete your account?",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                  );
+                },
+                child: const Text("Delete Account",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
