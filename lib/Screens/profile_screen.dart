44a269b80bf63d73e4f1b2c700927db90ede4dae
Adrian Szacsko <szacskoadrian@gmail.com>
2022-04-08 16:21:04 +0200

    add profile_screen.dart

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -23,12 +23,95 @@ class ProfileScreenState extends State<ProfileScreen> {
   static const routeName = '/settings-screen';
 
 
+  Widget information(String type, String text, IconData icon){
+    return Column(
+      children: [
+        Row(
+          children: [
+            const SizedBox(width: 20,),
+            Icon(
+              icon,
+              color: primaryColor,
+            ),
+            const SizedBox(width: 20,),
+            Text(type,
+              style: const TextStyle(
+                  fontSize: 20
+              ),
+            ),
+            const SizedBox(width: 20,),
+            Text(text,
+              style: const TextStyle(
+                  fontSize: 20
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 15,),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(context),
       bottomNavigationBar: myBottomAppBar(context),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: backgroundColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  children: const [
+                    CircleAvatar(
+                      backgroundImage: AssetImage("assets/Images/profile-unknown.png"),
+                      radius: 100,
+                    ),
+                    SizedBox(height: 20,),
+                    Text("Marko Stahovec",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Text("Comments: 5",
+                    style: TextStyle(
+                      fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50,),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "   Informations:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              const Divider(
+                color: Colors.black,
+                thickness: 1,
+              ),
+              information("Email:","xstahovec@stuba.sk",Icons.mail_rounded),
+              information("Study year:","2",Icons.book_outlined),
+              information("Register date:","2022.02.11",Icons.vpn_key_rounded),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
