d4bdd5d33dacba955577723e7bc009381305258c
AdrianSzacsko <xszacsko@stuba.sk>
2022-04-19 18:21:01 +0200

    modify info screen

--- lib/Screens/info_screen.dart
+++ lib/Screens/info_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:mtaa_frontend/Screens/components/sign_up_form.dart';
 import 'package:mtaa_frontend/Screens/profile_screen.dart';
 //import 'package:flutter/rendering.dart';
 import 'package:mtaa_frontend/UI/inputField.dart';
@@ -26,6 +27,7 @@ class InfoScreen extends StatefulWidget {
 class InfoScreenState extends State<InfoScreen> {
   static const routeName = '/info-screen';
   late VideoPlayerController _controller;
+  var tapped = false;
 
 
   @override
@@ -40,22 +42,123 @@ class InfoScreenState extends State<InfoScreen> {
       });
   }
 
+  Widget createCard(){
+    return Padding(
+      padding: const EdgeInsets.all(defaultPadding),
+      child: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            elevation:10,
+            shadowColor: primaryColor[300],
+            child:
+            Column(
+              children: [
+                Center(
+                  child:
+                  Padding(
+                      padding: const EdgeInsets.only(top: defaultPadding * 2),
+                      child: Image.asset('assets/Images/puzzle.png',
+                        height: 80.0,
+                        fit: BoxFit.cover,
+                      )
+                  ),
+                ),
+                const SizedBox(height: defaultPadding * 2),
+                Center(
+                  child:
+                  Padding(
+                      padding: const EdgeInsets.only(top: defaultPadding * 2),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(children: <TextSpan>[
+                          TextSpan(
+                              text: "Aca",
+                              style: TextStyle(
+                                  color: secondaryColor,
+                                  fontSize: 40)),
+                          TextSpan(
+                              text: "Rate",
+                              style: TextStyle(
+                                  color: primaryColor,
+                                  fontSize: 40)),
+                        ]),
+                      )
+                  ),
+                ),
+                const SizedBox(height: defaultPadding * 0.5),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: defaultPadding * 2),
+                    child: Text("An app made for an assignment in MTAA"),
+                  ),
+                ),
+                const SizedBox(height: defaultPadding * 1),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: defaultPadding),
+                    child: Text("Made by: Marko Stahovec, Adrian Szacsko",
+                    style: TextStyle(fontWeight: FontWeight.bold),),
+                  ),
+                ),
+                const SizedBox(height: 20,)
+
+              ],
+            ),
+          ),
+          /*Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+            ),
+          elevation:10,
+          shadowColor: secondaryColor[300],
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+            padding: EdgeInsets.only(left: defaultPadding * 2, top: defaultPadding),
+            child: Column(
+              children: const [
+                Text("Special thanks to...",
+                  style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text("Ing. Marek Galinski, PhD.")
+              ],
+            ),
+            ),
+          ),
+          ),*/
+        ],
+      ),
+
+
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: myAppBar(context),
-    bottomNavigationBar: myBottomAppBar(context),
-    body: Center(
-      child: _controller.value.isInitialized
+        bottomNavigationBar: myBottomAppBar(context),
+        body: !tapped ? Center(
+          child: _controller.value.isInitialized
           ? AspectRatio(
-        aspectRatio: _controller.value.aspectRatio,
-        child: VideoPlayer(_controller),
-      )
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+          )
           : Container(),
-    ),
-      floatingActionButton: FloatingActionButton(
+    ) :
+            createCard(),
+      floatingActionButton: !tapped ? FloatingActionButton(
         heroTag: null,
         onPressed: () {
+            _controller.pause();
+          setState(() {
+            tapped = true;
+          });
           /*setState(() {
             _controller.play();
           });*/
@@ -63,7 +166,7 @@ class InfoScreenState extends State<InfoScreen> {
         child: Icon(
           _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
-      ),
+      ) : null,
     );
   }
 
